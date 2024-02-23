6ed794d7755e4961fe6b35a01909f9f6ec28affa
Ivana Cardoso <ivanawaters@gmail.com>
2024-02-23 13:34:18 -0300

    Code update

--- RCode.R
+++ RCode.R
@@ -83,15 +83,69 @@ NP_FC_plot =
   labs(x = "Number of forest patches",
        y = "Forest cover (%)") +
   geom_point() +
-  theme_pubr(base_size = 20)+
-  geom_hline(yintercept = c(20, 50))
+  geom_hline(yintercept = c(20, 50))+
+  theme_pubr(base_size = 20)
+  
 ggsave("Number_patches_vs_Forest_cover.png", plot=NP_FC_plot,
        width = 200, height = 200, units = "mm",
        dpi = 600)
 
-# Select landscapes with 20 to 50% forest cover because, within these values, forest cover is visually not correlated with number of forest fragments
+
+# Select landscapes with 20 to 40% forest cover because, within these values, forest cover is visually not correlated with number of forest fragments
 metrics = subset(metrics, metrics$forest_cover <= 50 & metrics$forest_cover >= 20)
 cor(metrics$number_patches, metrics$forest_cover) # not correlated (r=-0.32)
+selected_landscapes <- subset(metrics, 
+                              metrics$id == 145 | metrics$id == 583 | 
+                              metrics$id == 503 | metrics$id == 106 |
+                              metrics$id == 112 | metrics$id == 428 |
+                              metrics$id == 259 | metrics$id == 468 |
+                              metrics$id == 143 | metrics$id == 229 |
+                              metrics$id == 422 | metrics$id == 381 |
+                              metrics$id == 188 | metrics$id == 228 |
+                              metrics$id == 105 | metrics$id == 28 |
+                              metrics$id == 226 | metrics$id == 186 |
+                              metrics$id == 376 | metrics$id == 128 |
+                              metrics$id == 624 | metrics$id == 545 |
+                              metrics$id == 585 | metrics$id == 586 |
+                              metrics$id == 546 | metrics$id == 76 |
+                              metrics$id == 299 | metrics$id == 588 |
+                              metrics$id == 387 | metrics$id == 427)
+
+descarted_landscapes <- subset(metrics, metrics$id == 690 | metrics$id == 727 | 
+                                metrics$id == 646 | metrics$id == 654 |
+                                 metrics$id == 789 | metrics$id == 728 |
+                                 metrics$id == 489 | metrics$id == 331 |
+                                 metrics$id == 749 | metrics$id == 693 |
+                                 metrics$id == 687 | metrics$id == 614 |
+                                 metrics$id == 452)
+
+NP_FC_plot +
+geom_point(selected_landscapes, mapping = aes(x=number_patches, y=forest_cover), col = "red")+
+geom_point(descarted_landscapes, mapping = aes(x=number_patches, y=forest_cover), col = "blue")
+
+
+paired_landscapes <- read.csv("paired_landscapes.csv")
+paired_landscapes$group <- as.factor(paired_landscapes$group)
+
+paired = 
+  ggplot(data = paired_landscapes,
+       mapping = aes(x = number_patches, y = forest_cover,
+                     group = group)) +
+  labs(x = "Number of forest patches",
+       y = "Forest cover (%)") +
+  geom_point(size = 3, shape = 16) +
+  geom_line()+
+  theme_pubr(base_size = 20)
+
+paired +
+  geom_point(data = paired_landscapes[21:24,],
+             mapping = aes(x = number_patches, y = forest_cover),
+             color = "red")
+
+correl = paired_landscapes[1:20,]
+
+cor(correl$number_patches, correl$forest_cover)
+
 
 
 # Export data to visualize in QGIS
@@ -106,3 +160,4 @@ plot(landscapes, add=T)
 
 writeRaster(Forest_formation, "Floresta_Balbina", format="GTiff")
 st_write(landscapes, "landscapes.shp", append = F)
+write.csv(selected_landscapes, "selected_landscapes.csv")
