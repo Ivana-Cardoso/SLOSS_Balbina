f8a26ba5b5b34bc8484734d12c761deb252d961d
Ivana Cardoso <ivanawaters@gmail.com>
2025-09-10 14:53:55 -0400

    updates

--- RCode_analysis.R
+++ RCode_analysis.R
@@ -4,9 +4,11 @@
 # Author: Ivana Cardoso - ivanawaters@gmail.com
 #
 # Created: September 09, 2025
-# Last modification: September 09, 2025
+# Last modification: September 10, 2025
 
 library(dplyr)
+library(car)
+library(ggplot2)
 
 # Importing data
 rm(list = ls()); gc()
@@ -44,8 +46,51 @@ comm_landscape <-
   comm_landscape %>% 
   mutate(across(where(is.numeric), ~ ifelse(. > 0, 1, 0)))
 
-
 landscapes$richness <- rowSums(comm_landscape[,2:171]) # richness by landscape
 
+landscapes$type <- NA
+landscapes$type[1:8] <- "FL"
+landscapes$type[9:12] <- "MS"
+landscapes$type[13:20] <- "SS"
+landscapes$type <- as.factor(landscapes$type)
 
+# Are there differences in ant species richness (FL vs MS vs SS)? ANOVA
+mod <- aov(richness~type, data = landscapes)
+summary(mod) # F-value = 1.95, p-value = 0.17
+TukeyHSD(mod) # There are NO differences
+
+shapiro.test(mod$residuals) # normal residuals (p=0.81)
+leveneTest(mod) # variances are equal (p=0.64)
+
+# Plotting the graph
+plot1 <-
+ggplot(data = landscapes,
+       mapping = aes(x = type, y = richness)) +
+  geom_boxplot(outlier.shape = NA) +
+  geom_jitter(size = 2.5, alpha = 0.6) +
+  xlab("Landscape type") +
+  ylab("Number of ant species") +
+  
+  theme_bw(base_size = 10) +
+  
+  theme(panel.grid = element_blank(),
+        panel.border = element_rect(colour = "black"),
+        axis.title = element_text(colour = "black", face = "bold"),
+        axis.text = element_text(colour = "black"),
+        axis.ticks = element_line(colour = "black", size = 0.25)) +
+  
+  annotate("text", x = 3, y = 53,
+           hjust = 0, vjust = 0, size = 2.5,
+           parse = T, label = as.character(expression(italic(F)*"-value = 1.95"))) +
+  
+  annotate("text", x = 3, y = 53,
+           hjust = 0, vjust = 2, size = 2.5,
+           parse = T, label = as.character(expression(italic(p)*"-value = 0.17")))
+
+setwd("C:/Users/ivana/OneDrive/PhD_INPA/2.SLOSS_question/Analises/SLOSS_Balbina/figures")
+ggsave(
+  plot = plot1,
+  filename = "Richness_LandscapeType.png", dpi = 500,
+  width = 8 * 1.5, height = 8, units = 'cm')   
+  
 
