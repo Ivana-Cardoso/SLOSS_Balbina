c9b53d552a7b0aff242715f329809644ab8f3264
Ivana Cardoso <ivanawaters@gmail.com>
2025-09-10 15:23:53 -0400

    updates

--- RCode_analysis.R
+++ RCode_analysis.R
@@ -54,7 +54,10 @@ landscapes$type[9:12] <- "MS"
 landscapes$type[13:20] <- "SS"
 landscapes$type <- as.factor(landscapes$type)
 
-# Are there differences in ant species richness (FL vs MS vs SS)? ANOVA
+
+
+# Are there differences in ant species richness (FL vs MS vs SS)? 
+# ANOVA
 mod <- aov(richness~type, data = landscapes)
 summary(mod) # F-value = 1.95, p-value = 0.17
 TukeyHSD(mod) # There are NO differences
@@ -94,3 +97,51 @@ ggsave(
   width = 8 * 1.5, height = 8, units = 'cm')   
   
 
+
+# Are there differences in ant species richness (Richness vs Number of patches)? 
+# Linear regression
+mod2 <- lm(richness~number_patches, data = landscapes) 
+summary(mod2) # adj-r2 = 0.20, slope = -0.21, p = 0.03
+
+shapiro.test(mod2$residuals) # normal residuals (p=0.90)
+
+# Plotting the graph
+plot2 <-
+  ggplot(data = landscapes,
+         mapping = aes(x = number_patches, y = richness)) +
+  
+  geom_smooth(method = "lm", color = "black") +
+  geom_point(size = 2.5, alpha = 0.6) +
+  
+  labs(x = "Number of patches in the landscape",
+       y = "Number of ant species") +
+  
+  theme_bw(base_size = 10) +
+  
+  theme(panel.grid = element_blank(),
+        panel.border = element_rect(colour = "black"),
+        axis.title = element_text(colour = "black", face = "bold"),
+        axis.text = element_text(colour = "black"),
+        axis.ticks = element_line(colour = "black", size = 0.25)) +
+  
+  annotate("text", x = 50, y = 51,
+           hjust = 0, vjust = 0, size = 2.5,
+           parse = T,
+           label = as.character(expression(italic(r)^{2}*""[adj]*" = 0.20"))) +
+  
+  annotate("text", x = 50, y = 51,
+           hjust = 0, vjust = 2, size = 2.5,
+           parse = T,
+           label = as.character(expression(italic()*"slope = -0.21"))) +
+  
+  annotate("text", x = 50, y = 51,
+           hjust = 0, vjust = 4, size = 2.5,
+           parse = T, label = as.character(expression(italic(p)*"-value = 0.03")))
+
+plot2
+
+setwd("C:/Users/ivana/OneDrive/PhD_INPA/2.SLOSS_question/Analises/SLOSS_Balbina/figures")
+ggsave(
+  plot = plot2,
+  filename = "Richness_NumberPatches.png", dpi = 500,
+  width = 8 * 1.5, height = 8, units = 'cm')   
